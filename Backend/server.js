fce036bad69ecf92bccb8b91e5b2e0dbd3fd4dc2
HPRINCE80 <princeyaduvanshi226@gmail.com>
2026-08-09 19:54:31 +0530

    L

--- Backend/server.js
+++ Backend/server.js
@@ -8,9 +8,11 @@ const invokeGeminiAi = require("./src/services/ai.services");
 const app = require("./src/app");
 const connectToDB = require("./src/config/database");
 
+const PORT = process.env.PORT || 3000;
+
 connectToDB();
 // invokeGeminiAi();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
