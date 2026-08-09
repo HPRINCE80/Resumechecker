404686f3b4bc2b180320b8c2468f415620921abe
HPRINCE80 <princeyaduvanshi226@gmail.com>
2026-08-09 17:22:59 +0530

    deploy

--- Fronted/src/features/interview/Services/interview.api.js
+++ Fronted/src/features/interview/Services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://resumechecker-backend-m0ww.onrender.com",
     withCredentials: true,
 })
 
