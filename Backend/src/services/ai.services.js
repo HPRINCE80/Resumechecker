f070ae2abc706ec79b084976989d26f658220772
HPRINCE80 <princeyaduvanshi226@gmail.com>
2026-08-22 21:49:06 +0530

    l

--- Backend/src/services/ai.services.js
+++ Backend/src/services/ai.services.js
@@ -69,7 +69,7 @@ async function generateInterviewReport({ resume, selfDescription, jobDescription
 
     try {
         const response = await ai.models.generateContent({
-            model:  "gemini-3.5-flash-lite",
+            model:  "gemini-2.5-flash-lite",
             contents: prompt,
             config: {
                 responseMimeType: "application/json",
